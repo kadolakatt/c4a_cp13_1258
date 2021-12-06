0bf3875ce28338a65e79bd8025d85bb9f2652f36
kadolakatt <klacatt@uninorte.edu.co>
2021-12-06 14:08:45 -0500

    Corrección parseInt parámetros limit y page.

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 const port = config.SERVER_PORT;
 
 app.get('/projects/all', async (request, response) => {
-    const page = request.query.page;
-    const limit = request.query.limit;
-    const datos = await listarProyectos(parseInt(limit), page);
+    const page = parseInt(request.query.page);
+    const limit = parseInt(request.query.limit);
+    const datos = await listarProyectos(limit, page);
     response.json(datos);
 });
 
